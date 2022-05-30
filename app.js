dce42f4857fa785dcde6d984b22388f912dcf5d1
Akorsikov <akors@mail.ru>
2022-05-30 14:43:59 +0300

    refactor: correct the names of identifiers, classes & selectors into html,css,js-files according to rules 3.2-3.4 in html-and-css.md

--- app.js
+++ app.js
@@ -13,14 +13,15 @@ var addButton=document.getElementsByTagName("button")[0];//first button
 var incompleteTaskHolder=document.getElementById("incomplete-tasks");//ul of #incomplete-tasks
 var completedTasksHolder=document.getElementById("completed-tasks");//completed-tasks
 
-
 //New task list item
 var createNewTaskElement=function(taskString){
 
     var listItem=document.createElement("li");
+        listItem.classList.add('item-list-tasks');
 
     //input (checkbox)
     var checkBox=document.createElement("input");//checkbx
+        checkBox.classList.add('input');
     //label
     var label=document.createElement("label");//label
     //input (text)
@@ -33,18 +34,19 @@ var createNewTaskElement=function(taskString){
     var deleteButtonImg=document.createElement("img");//delete button image
 
     label.innerText=taskString;
-    label.className='task';
+    label.className='label task';
 
     //Each elements, needs appending
     checkBox.type="checkbox";
     editInput.type="text";
-    editInput.className="task";
+    editInput.className="input task";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-    editButton.className="edit";
+    editButton.className="btn edit";
 
-    deleteButton.className="delete";
+    deleteButton.className="btn delete";
     deleteButtonImg.src='./remove.svg';
+    deleteButtonImg.classList.add('remove-img');
     deleteButton.appendChild(deleteButtonImg);
 
 
@@ -83,7 +85,7 @@ var editTask=function(){
     var listItem=this.parentNode;
 
     var editInput=listItem.querySelector('input[type=text]');
-    var label=listItem.querySelector("label");
+    var label=listItem.querySelector(".label");
     var editBtn=listItem.querySelector(".edit");
     var containsClass=listItem.classList.contains("edit-mode");
     //If class of the parent is .edit-mode
